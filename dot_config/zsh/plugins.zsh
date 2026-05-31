0ef03fdb1b5ed8bd3c67201774c3b73f747acb24
Tyler Cash <12643375+Tyler-Cash@users.noreply.github.com>
2026-05-31 20:57:48 +1000

    feat: add completion strategy to zsh-autosuggestions for predictive suggestions

--- dot_config/zsh/plugins.zsh
+++ dot_config/zsh/plugins.zsh
@@ -2,6 +2,7 @@
 fpath=("$(brew --prefix)/share/zsh-completions" $fpath)
 
 # Plugins
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 source "$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 source "$(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 source "$(brew --prefix)/share/zsh-history-substring-search/zsh-history-substring-search.zsh"
